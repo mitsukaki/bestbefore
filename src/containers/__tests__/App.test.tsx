2b35e89d50947e9a190888ef8e215828638f8407
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2023-03-25 12:07:26 -0600

    update unit tests

--- src/containers/__tests__/App.test.tsx
+++ src/containers/__tests__/App.test.tsx
@@ -1,13 +1,16 @@
 import { render } from '@testing-library/react';
+import ReduxProvider from 'containers/ReduxProvider';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 
 describe('<App />', () => {
   it('should render properly', () => {
     render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
+      <ReduxProvider>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </ReduxProvider>,
     );
   });
 });
