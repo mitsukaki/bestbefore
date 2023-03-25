fda1d63e8cd973ba1f29a16c92164eef92d51cb4
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2023-03-25 00:32:26 -0600

    update unit test for App

--- src/containers/__tests__/App.test.tsx
+++ src/containers/__tests__/App.test.tsx
@@ -1,8 +1,13 @@
-import { render } from 'utils/test';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 
 describe('<App />', () => {
   it('should render properly', () => {
-    render(<App />);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
   });
 });
